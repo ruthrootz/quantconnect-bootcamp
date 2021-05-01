05448e7ff61d0ee3cbb357f1f249a1b152f988c7
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-01 15:31:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-class BootcampAlgorithm(QCAlgorithm):
+class BootCampTask(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2020, 10, 28)  # Set Start Date
-        self.SetCash(100000)  # Set Strategy Cash
-        # self.AddEquity("SPY", Resolution.Minute)
-
-
+        self.AddEquity("SPY", Resolution.Daily)
+        self.SetCash(25000)
+        
     def OnData(self, data):
-        '''OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
-            Arguments:
-                data: Slice object keyed by symbol containing the stock data
-        '''
-
-        # if not self.Portfolio.Invested:
-        #    self.SetHoldings("SPY", 1)
+        pass
