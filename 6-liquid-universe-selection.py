2fa3a7a8cb6b7872fb75ad18946cf001eda097e3
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-04 09:39:41 -0400

    Update 6-liquid-universe-selection.py

--- 6-liquid-universe-selection.py
+++ 6-liquid-universe-selection.py
@@ -1,1 +1,25 @@
+class LiquidUniverseSelection(QCAlgorithm):
+    
+    filteredByPrice = None
+    
+    def Initialize(self):
+        self.SetStartDate(2019, 1, 11)  
+        self.SetEndDate(2019, 7, 1) 
+        self.SetCash(100000)  
+        self.AddUniverse(self.CoarseSelectionFilter)
+        self.UniverseSettings.Resolution = Resolution.Daily
+        self.UniverseSettings.Leverage = 2
+       
+    def CoarseSelectionFilter(self, coarse):
+        sortedByDollarVolume = sorted(coarse, key=lambda c: c.DollarVolume, reverse=True)
+        filteredByPrice = [c.Symbol for c in sortedByDollarVolume if c.Price > 10]
+        return filteredByPrice[:10] 
 
+    def OnSecuritiesChanged(self, changes):
+        self.changes = changes
+        self.Log(f"OnSecuritiesChanged({self.Time}):: {changes}")
+        for security in self.changes.RemovedSecurities:
+            if security.Invested:
+                self.Liquidate(security.Symbol)
+        for security in self.changes.AddedSecurities:
+            self.SetHoldings(security.Symbol, 0.18)
