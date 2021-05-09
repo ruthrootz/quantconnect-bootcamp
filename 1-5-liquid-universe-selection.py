637a41e3382345d336e722870f3a3e3de8ba9725
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-08 23:14:33 -0400

    Update 1-5-liquid-universe-selection.py

--- 1-5-liquid-universe-selection.py
+++ 1-5-liquid-universe-selection.py
@@ -1,25 +1,44 @@
-class LiquidUniverseSelection(QCAlgorithm):
+from datetime import timedelta
+
+class MOMAlphaModel(AlphaModel):
     
-    filteredByPrice = None
+    def __init__(self):
+        self.mom = []
+      
+    def OnSecuritiesChanged(self, algorithm, changes):
+        
+        # 1. Initialize a 14-day momentum indicator for each symbol
+        for security in changes.AddedSecurities:
+            symbol = security.Symbol
+            self.mom.append({"symbol":symbol, "indicator":algorithm.MOM(symbol, 14, Resolution.Daily)})
+        
+    def Update(self, algorithm, data):
+
+        #2. Sort the list of dictionaries by indicator in descending order
+        ordered = sorted(self.mom, key=lambda s: s["indicator"], reverse=True)
+        
+        #3. Return a group of insights, emitting InsightDirection.Up for the first item of ordered, and InsightDirection.Flat for the second
+        return Insight.Group([
+            # Create a grouped insight
+            Insight.Price(ordered[0]["symbol"], timedelta(1), InsightDirection.Up), 
+            Insight.Price(ordered[1]["symbol"], timedelta(1), InsightDirection.Flat)
+         ])
+        
+class FrameworkAlgorithm(QCAlgorithm):
     
     def Initialize(self):
-        self.SetStartDate(2019, 1, 11)  
-        self.SetEndDate(2019, 7, 1) 
-        self.SetCash(100000)  
-        self.AddUniverse(self.CoarseSelectionFilter)
-        self.UniverseSettings.Resolution = Resolution.Daily
-        self.UniverseSettings.Leverage = 2
-       
-    def CoarseSelectionFilter(self, coarse):
-        sortedByDollarVolume = sorted(coarse, key=lambda c: c.DollarVolume, reverse=True)
-        filteredByPrice = [c.Symbol for c in sortedByDollarVolume if c.Price > 10]
-        return filteredByPrice[:10] 
 
-    def OnSecuritiesChanged(self, changes):
-        self.changes = changes
-        self.Log(f"OnSecuritiesChanged({self.Time}):: {changes}")
-        for security in self.changes.RemovedSecurities:
-            if security.Invested:
-                self.Liquidate(security.Symbol)
-        for security in self.changes.AddedSecurities:
-            self.SetHoldings(security.Symbol, 0.18)
+        self.SetStartDate(2013, 10, 1)   
+        self.SetEndDate(2013, 12, 1)   
+        self.SetCash(100000)           
+        symbols = [Symbol.Create("SPY", SecurityType.Equity, Market.USA), Symbol.Create("BND", SecurityType.Equity, Market.USA)]
+        self.UniverseSettings.Resolution = Resolution.Daily
+        self.SetUniverseSelection(ManualUniverseSelectionModel(symbols))
+
+        # Call the MOMAlphaModel Class 
+        self.SetAlpha(MOMAlphaModel())
+
+        self.SetPortfolioConstruction(NullPortfolioConstructionModel())
+        self.SetRiskManagement(NullRiskManagementModel())
+        self.SetExecution(NullExecutionModel())
+        
