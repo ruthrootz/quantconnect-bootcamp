c8da0c69a844410bf200a4d5279f404d08c38ec6
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-08 22:55:28 -0400

    Update 1-8-the-algorithmic-framework.py

--- 1-8-the-algorithmic-framework.py
+++ 1-8-the-algorithmic-framework.py
@@ -1,22 +1,20 @@
 class FrameworkAlgorithm(QCAlgorithm):
     
     def Initialize(self):
-        
+
         self.SetStartDate(2013, 10, 1)   
         self.SetEndDate(2013, 12, 1)    
-        self.SetCash(100000)
-
-        #1. Set the NullUniverseSelectionModel()
-        self.SetUniverseSelection(NullUniverseSelectionModel())
+        self.SetCash(100000)           
         
-        #2. Set the NullAlphaModel()
+        #1. Create a SPY and BND Symbol object that gets passed to the Universe Selection Model
+        self.symbols = [Symbol.Create("SPY", SecurityType.Equity, Market.USA), Symbol.Create("BND", SecurityType.Equity, Market.USA)]
+        #2. Set the resolution of the universe assets to daily resolution
+        self.UniverseSettings.Resolution = Resolution.Daily
+        #3. Set a universe using self.SetUniverseSelection(), and pass in a ManualUniverseSelectionModel() 
+        # initialized with the symbols list
+        self.SetUniverseSelection(ManualUniverseSelectionModel(self.symbols))
         self.SetAlpha(NullAlphaModel())
-
-        #3. Set the NullPortfolioConstructionModel()
         self.SetPortfolioConstruction(NullPortfolioConstructionModel())
-
-        #4. Set the NullRiskManagementModel()
         self.SetRiskManagement(NullRiskManagementModel())
-
-        #5. Set the NullExecutionModel()
         self.SetExecution(NullExecutionModel())
+        
