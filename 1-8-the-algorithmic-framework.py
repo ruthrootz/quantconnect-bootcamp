71dd4ab74f5eeca74446dd8cd58720e2a77eb62e
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-08 22:51:48 -0400

    Update 1-8-the-algorithmic-framework.py

--- 1-8-the-algorithmic-framework.py
+++ 1-8-the-algorithmic-framework.py
@@ -1,1 +1,22 @@
+class FrameworkAlgorithm(QCAlgorithm):
+    
+    def Initialize(self):
+        
+        self.SetStartDate(2013, 10, 1)   
+        self.SetEndDate(2013, 12, 1)    
+        self.SetCash(100000)
 
+        #1. Set the NullUniverseSelectionModel()
+        self.SetUniverseSelection(NullUniverseSelectionModel())
+        
+        #2. Set the NullAlphaModel()
+        self.SetAlpha(NullAlphaModel())
+
+        #3. Set the NullPortfolioConstructionModel()
+        self.SetPortfolioConstruction(NullPortfolioConstructionModel())
+
+        #4. Set the NullRiskManagementModel()
+        self.SetRiskManagement(NullRiskManagementModel())
+
+        #5. Set the NullExecutionModel()
+        self.SetExecution(NullExecutionModel())
