0d1f8044e1a0d3e619b2f4a266ac79f3762739f6
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-08 22:39:09 -0400

    Update 1-7-200-50-ema-momentum-universe.py

--- 1-7-200-50-ema-momentum-universe.py
+++ 1-7-200-50-ema-momentum-universe.py
@@ -6,32 +6,47 @@ class EMAMomentumUniverse(QCAlgorithm):
         self.SetCash(100000)
         self.UniverseSettings.Resolution = Resolution.Daily
         self.AddUniverse(self.CoarseSelectionFunction)
-    
-    def CoarseSelectionFunction(self, coarse):
-        sortedByDollarVolume = sorted(coarse, key=lambda c: c.DollarVolume, reverse=True) 
-        selected = [c.Symbol for c in sortedByDollarVolume if c.Price > 10][:10]
-        return selected
         
-    def OnSecuritiesChanged(self, changes):  
+        #1. Create our dictionary and save it to self.averages
+        self.averages = {}
+    
+    def CoarseSelectionFunction(self, universe):  
+        selected = []
+        universe = sorted(universe, key=lambda c: c.DollarVolume, reverse=True)  
+        universe = [c for c in universe if c.Price > 10][:100]
+        
+        # Create loop to use all the coarse data
+        for coarse in universe:  
+            symbol = coarse.Symbol 
+            
+            #2. Check if we've created an instance of SelectionData for this symbol
+            if symbol not in self.averages:
+                #3. Create a new instance of SelectionData and save to averages[symbol]
+                self.averages[symbol] = SelectionData()
+            #4. Update the symbol with the latest coarse.AdjustedPrice data
+            self.averages[symbol].update(self.Time, coarse.AdjustedPrice)
+            #5. Check if 50-EMA > 200-EMA and if so append the symbol to selected list.
+            if self.averages[symbol].fast > self.averages[symbol].slow:
+                if self.averages[symbol].is_ready():
+                    selected.append(symbol)
+        return selected[:10]
+        
+    def OnSecuritiesChanged(self, changes):
         for security in changes.RemovedSecurities:
-            self.Liquidate(security.Symbol) 
+            self.Liquidate(security.Symbol)
+       
         for security in changes.AddedSecurities:
             self.SetHoldings(security.Symbol, 0.10)
-
-#1. Create a class SelectionData
-class SelectionData:
-    #2. Create a constructor that takes self 
+            
+class SelectionData(object):
     def __init__(self):
-        #2. Save the fast and slow ExponentialMovingAverage
         self.slow = ExponentialMovingAverage(200)
         self.fast = ExponentialMovingAverage(50)
     
-    #3. Check if our indicators are ready
     def is_ready(self):
         return self.slow.IsReady and self.fast.IsReady
     
-    #4. Use the "indicator.Update" method to update the time and price of both indicators
     def update(self, time, price):
-        self.slow.Update(time, price)
         self.fast.Update(time, price)
+        self.slow.Update(time, price)
         
