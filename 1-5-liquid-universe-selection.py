12ac28fd26f7afa5eed5d2971ebaaa19ad29bb77
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-11 08:40:45 -0400

    Update 1-5-liquid-universe-selection.py

--- 1-5-liquid-universe-selection.py
+++ 1-5-liquid-universe-selection.py
@@ -1,44 +1,27 @@
 from datetime import timedelta
-
-class MOMAlphaModel(AlphaModel):
-    
+class MOMAlphaModel(AlphaModel): 
     def __init__(self):
         self.mom = []
-      
     def OnSecuritiesChanged(self, algorithm, changes):
-        
-        # 1. Initialize a 14-day momentum indicator for each symbol
         for security in changes.AddedSecurities:
             symbol = security.Symbol
             self.mom.append({"symbol":symbol, "indicator":algorithm.MOM(symbol, 14, Resolution.Daily)})
-        
     def Update(self, algorithm, data):
-
-        #2. Sort the list of dictionaries by indicator in descending order
-        ordered = sorted(self.mom, key=lambda s: s["indicator"], reverse=True)
-        
-        #3. Return a group of insights, emitting InsightDirection.Up for the first item of ordered, and InsightDirection.Flat for the second
-        return Insight.Group([
-            # Create a grouped insight
-            Insight.Price(ordered[0]["symbol"], timedelta(1), InsightDirection.Up), 
-            Insight.Price(ordered[1]["symbol"], timedelta(1), InsightDirection.Flat)
-         ])
-        
+        ordered = sorted(self.mom, key=lambda kv: kv["indicator"].Current.Value, reverse=True)
+        return Insight.Group([Insight.Price(ordered[0]['symbol'], timedelta(1), InsightDirection.Up), Insight.Price(ordered[1]['symbol'], timedelta(1), InsightDirection.Flat) ])
+ 
+         
 class FrameworkAlgorithm(QCAlgorithm):
-    
     def Initialize(self):
-
-        self.SetStartDate(2013, 10, 1)   
+        self.SetStartDate(2013, 10, 1)  
         self.SetEndDate(2013, 12, 1)   
         self.SetCash(100000)           
-        symbols = [Symbol.Create("SPY", SecurityType.Equity, Market.USA), Symbol.Create("BND", SecurityType.Equity, Market.USA)]
+        symbols = [Symbol.Create("SPY", SecurityType.Equity, Market.USA),  Symbol.Create("BND", SecurityType.Equity, Market.USA)]
         self.UniverseSettings.Resolution = Resolution.Daily
         self.SetUniverseSelection(ManualUniverseSelectionModel(symbols))
-
-        # Call the MOMAlphaModel Class 
         self.SetAlpha(MOMAlphaModel())
+        self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
 
-        self.SetPortfolioConstruction(NullPortfolioConstructionModel())
-        self.SetRiskManagement(NullRiskManagementModel())
-        self.SetExecution(NullExecutionModel())
+        #1. Set the Risk Management handler to use a 2% maximum drawdown
         
+        self.SetExecution(NullExecutionModel())
