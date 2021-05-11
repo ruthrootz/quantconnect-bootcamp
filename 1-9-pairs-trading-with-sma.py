cc4e6479b26474c24d92c31e0ab54870b5b1cc30
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-11 08:48:43 -0400

    Update 1-9-pairs-trading-with-sma.py

--- 1-9-pairs-trading-with-sma.py
+++ 1-9-pairs-trading-with-sma.py
@@ -1,1 +1,16 @@
+from datetime import timedelta, datetime
 
+class SMAPairsTrading(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2018, 7, 1)   
+        self.SetEndDate(2019, 3, 31)
+        self.SetCash(100000)
+        
+        #1. Using the ManualUniverseSelectionModel(), add the symbols "PEP" and "KO" 
+        symbols = ...
+        #2. In Universe Settings, set the resolution to hour
+        
+        self.AddAlpha(NullAlphaModel())
+        self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
+        self.SetExecution(ImmediateExecutionModel())
