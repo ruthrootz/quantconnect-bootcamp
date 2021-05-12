ee56e97efe22c69a9a295daac60bd13751a1f26e
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-12 10:47:15 -0400

    Update 1-9-pairs-trading-with-sma.py

--- 1-9-pairs-trading-with-sma.py
+++ 1-9-pairs-trading-with-sma.py
@@ -8,8 +8,10 @@ class SMAPairsTrading(QCAlgorithm):
         self.SetCash(100000)
         
         #1. Using the ManualUniverseSelectionModel(), add the symbols "PEP" and "KO" 
-        symbols = ...
+        symbols = [Symbol.Create("PEP", SecurityType.Equity, Market.USA), Symbol.Create("KO", SecurityType.Equity, Market.USA)]
+        self.AddUniverseSelection(ManualUniverseSelectionModel(symbols))
         #2. In Universe Settings, set the resolution to hour
+        self.UniverseSettings.Resolution = Resolution.Hour
         
         self.AddAlpha(NullAlphaModel())
         self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
