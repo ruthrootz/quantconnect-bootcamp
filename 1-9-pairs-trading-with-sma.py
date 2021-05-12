dba21441f56c3111194f3396b6f5dd5da51976a2
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-12 10:53:56 -0400

    Update 1-9-pairs-trading-with-sma.py

--- 1-9-pairs-trading-with-sma.py
+++ 1-9-pairs-trading-with-sma.py
@@ -7,12 +7,29 @@ class SMAPairsTrading(QCAlgorithm):
         self.SetEndDate(2019, 3, 31)
         self.SetCash(100000)
         
-        #1. Using the ManualUniverseSelectionModel(), add the symbols "PEP" and "KO" 
         symbols = [Symbol.Create("PEP", SecurityType.Equity, Market.USA), Symbol.Create("KO", SecurityType.Equity, Market.USA)]
         self.AddUniverseSelection(ManualUniverseSelectionModel(symbols))
-        #2. In Universe Settings, set the resolution to hour
         self.UniverseSettings.Resolution = Resolution.Hour
         
-        self.AddAlpha(NullAlphaModel())
+        #1. Create an instance of the PairsTradingAlphamodel()
+        self.AddAlpha(PairsTradingAlphaModel())
+        
         self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
         self.SetExecution(ImmediateExecutionModel())
+         
+class PairsTradingAlphaModel(AlphaModel): 
+
+    def __init__(self):
+        #2. Initialize an empty list self.pair = [ ]
+        self.pair = []
+        
+    def Update(self, algorithm, data): 
+        
+        #3. Set the price difference calculation to spread.
+        spread = self.pair[1].Price - self.pair[0].Price
+        return []
+    
+    def OnSecuritiesChanged(self, algorithm, changes):
+        
+        #4. Set self.pair to the changes.AddedSecurities changes
+        self.pair = [s for s in changes.AddedSecurities]
