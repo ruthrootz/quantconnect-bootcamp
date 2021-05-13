8c2b44ba46edb2e575c0fa25cdfe1ed476aa1644
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-13 13:16:55 -0400

    Update 1-10-liquid-value-stocks.py

--- 1-10-liquid-value-stocks.py
+++ 1-10-liquid-value-stocks.py
@@ -1,4 +1,3 @@
-import datetime
 from datetime import timedelta
 from QuantConnect.Data.UniverseSelection import * 
 from Selection.FundamentalUniverseSelectionModel import FundamentalUniverseSelectionModel
@@ -9,28 +8,36 @@ class LiquidValueStocks(QCAlgorithm):
         self.SetStartDate(2016, 10, 1)
         self.SetEndDate(2017, 10, 1)
         self.SetCash(100000)
+        self.universe = None
         self.UniverseSettings.Resolution = Resolution.Hour
         self.AddUniverseSelection(LiquidValueUniverseSelectionModel())
         self.AddAlpha(NullAlphaModel())
         self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
         self.SetExecution(ImmediateExecutionModel())
-
+    
 class LiquidValueUniverseSelectionModel(FundamentalUniverseSelectionModel):
     
     def __init__(self):
-        super().__init__(True, None, None)
         self.lastMonth = -1 
+        super().__init__(True, None, None)
     
     def SelectCoarse(self, algorithm, coarse):
-        
-        #1. If it isn't time to update data, return the previous symbols 
         if self.lastMonth == algorithm.Time.month:
             return Universe.Unchanged
-        #2. Update self.lastMonth with current month to make sure only process once per month
         self.lastMonth = algorithm.Time.month
-        #3. Sort symbols by dollar volume and if they have fundamental data, in descending order
-        sortedByDollarVolume = sorted([s for s in coarse if s.HasFundamentalData],
-        key=lambda s: s.DollarVolume, reverse=True)
+
+        sortedByDollarVolume = sorted([x for x in coarse if x.HasFundamentalData],
+            key=lambda x: x.DollarVolume, reverse=True)
+
+        return [x.Symbol for x in sortedByDollarVolume[:100]]
+
+    def SelectFine(self, algorithm, fine):
+        #1. Sort yields per share
+        sortedByYields = sorted(fine, key=lambda s: s.ValuationRatios.EarningYield, reverse=True) 
         
-        #4. Return the top 100 Symbols by Dollar Volume
-        return [s.Symbol for s in sortedByDollarVolume][:100]
+        #2. Take top 10 most profitable stocks -- and bottom 10 least profitable stocks
+        # Save to the variable self.universe
+        self.universe = sortedByYields[:10] + sortedByYields[-10:]
+        
+        #3. Return the symbol objects by iterating through self.universe with list comprehension
+        return [f.Symbol for f in self.universe]
