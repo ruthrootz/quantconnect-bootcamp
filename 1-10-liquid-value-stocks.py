f052dfc3403935cfc090d771d01c8ae3fb3fcd05
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-13 13:10:50 -0400

    Update 1-10-liquid-value-stocks.py

--- 1-10-liquid-value-stocks.py
+++ 1-10-liquid-value-stocks.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import datetime
+from datetime import timedelta
 from QuantConnect.Data.UniverseSelection import * 
 from Selection.FundamentalUniverseSelectionModel import FundamentalUniverseSelectionModel
 
@@ -8,26 +9,28 @@ class LiquidValueStocks(QCAlgorithm):
         self.SetStartDate(2016, 10, 1)
         self.SetEndDate(2017, 10, 1)
         self.SetCash(100000)
-        self.AddAlpha(NullAlphaModel())
-        
-        #1. Create an instance of our LiquidValueUniverseSelectionModel and set to hourly resolution
-        self.AddUniverseSelection(LiquidValueUniverseSelectionModel())
         self.UniverseSettings.Resolution = Resolution.Hour
-        
+        self.AddUniverseSelection(LiquidValueUniverseSelectionModel())
+        self.AddAlpha(NullAlphaModel())
         self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
         self.SetExecution(ImmediateExecutionModel())
-        
-        
-# Define the Universe Model Class
+
 class LiquidValueUniverseSelectionModel(FundamentalUniverseSelectionModel):
     
     def __init__(self):
         super().__init__(True, None, None)
+        self.lastMonth = -1 
     
-    #2. Add an empty SelectCoarse() method with its parameters
     def SelectCoarse(self, algorithm, coarse):
-        return Universe.Unchanged
-    
-    #2. Add an empty SelectFine() method with is parameters
-    def SelectFine(self, algorithm, fine):
-        return Universe.Unchanged
+        
+        #1. If it isn't time to update data, return the previous symbols 
+        if self.lastMonth == algorithm.Time.month:
+            return Universe.Unchanged
+        #2. Update self.lastMonth with current month to make sure only process once per month
+        self.lastMonth = algorithm.Time.month
+        #3. Sort symbols by dollar volume and if they have fundamental data, in descending order
+        sortedByDollarVolume = sorted([s for s in coarse if s.HasFundementalData],
+        key=lambda s: s.DollarVolume, reverse=True)
+        
+        #4. Return the top 100 Symbols by Dollar Volume
+        return [s.Symbol for s in sortedByDollarVolume][:100]
