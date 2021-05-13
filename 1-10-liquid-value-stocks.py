97f9cc553f03479aa548b64350fcd558125eceb0
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-13 13:03:12 -0400

    Update 1-10-liquid-value-stocks.py

--- 1-10-liquid-value-stocks.py
+++ 1-10-liquid-value-stocks.py
@@ -1,1 +1,32 @@
+from datetime import datetime
+from QuantConnect.Data.UniverseSelection import * 
+from Selection.FundamentalUniverseSelectionModel import FundamentalUniverseSelectionModel
 
+class LiquidValueStocks(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2016, 10, 1)
+        self.SetEndDate(2017, 10, 1)
+        self.SetCash(100000)
+        self.AddAlpha(NullAlphaModel())
+        
+        #1. Create an instance of our LiquidValueUniverseSelectionModel and set to hourly resolution
+        self.AddUniverseSelection(LiquidValueUniverseSelectionModel())
+        
+        self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
+        self.SetExecution(ImmediateExecutionModel())
+        
+        
+# Define the Universe Model Class
+class LiquidValueUniverseSelectionModel(FundamentalUniverseSelectionModel):
+    
+    def __init__(self):
+        super().__init__(True, None, None)
+    
+    #2. Add an empty SelectCoarse() method with its parameters
+    def SelectCoarse(self, algorithm, coarse):
+        pass
+    
+    #2. Add an empty SelectFine() method with is parameters
+    def SelectFine(self, algorithm, fine):
+        pass
