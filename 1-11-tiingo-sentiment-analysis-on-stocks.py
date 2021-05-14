14217e06a156d53743a953a1d84239d59750896a
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-14 16:35:01 -0400

    Update 1-11-tiingo-sentiment-analysis-on-stocks.py

--- 1-11-tiingo-sentiment-analysis-on-stocks.py
+++ 1-11-tiingo-sentiment-analysis-on-stocks.py
@@ -1,1 +1,27 @@
+#1. Import Tiingo Data 
+from datetime import datetime, timedelta
+import numpy as np
 
+class TiingoNewsSentimentAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2016, 11, 1)
+        self.SetEndDate(2017, 3, 1)  
+        
+        #2. Add AAPL and NKE symbols to a Manual Universe 
+        symbols = [Symbol.Create("AAPL", SecurityType.Equity, Market.USA), Symbol.Create("NKE", SecurityType.Equity, Market.USA)]
+        self.SetUniverseSelection(ManualUniverseSelectionModel(symbols))
+        # 3. Add an instance of the NewsSentimentAlphaModel
+        self.SetAlpha(NewsSentimentAlphaModel())
+        self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel()) 
+        self.SetExecution(ImmediateExecutionModel()) 
+        self.SetRiskManagement(NullRiskManagementModel())
+        
+# 4. Create a NewsSentimentAlphaModel class with Update() and OnSecuritiesChanged() methods
+class NewsSentimentAlphaModel(QCAlgorithm)
+
+    def Update(self, algorithm):
+        pass
+    
+    def OnSecuritiesChanged(self, algorithm):
+        pass
