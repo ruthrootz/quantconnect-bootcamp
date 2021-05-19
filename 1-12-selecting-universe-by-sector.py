8c545f8eccac7cc845a79b6decd07a2caee5e76f
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-19 17:16:13 -0400

    Update 1-12-selecting-universe-by-sector.py

--- 1-12-selecting-universe-by-sector.py
+++ 1-12-selecting-universe-by-sector.py
@@ -9,12 +9,10 @@ class SectorBalancedPortfolioConstruction(QCAlgorithm):
         self.SetStartDate(2016, 12, 28) 
         self.SetEndDate(2017, 3, 1) 
         self.SetCash(100000) 
-
         self.UniverseSettings.Resolution = Resolution.Hour
         self.SetUniverseSelection(MyUniverseSelectionModel())
         self.SetAlpha(ConstantAlphaModel(InsightType.Price, InsightDirection.Up, timedelta(1), 0.025, None))
-        # 1. Set an instance of the MySectorWeightingPortfolioConstructionModel using self.SetPortfolioConstruction
-        self.SetPortfolioConstruction(MySectorWeightingPortfolioConstructionModel())
+        self.SetPortfolioConstruction(MySectorWeightingPortfolioConstructionModel(Resolution.Daily))
         self.SetExecution(ImmediateExecutionModel())
 
 class MyUniverseSelectionModel(FundamentalUniverseSelectionModel):
@@ -35,13 +33,34 @@ class MyUniverseSelectionModel(FundamentalUniverseSelectionModel):
         filtered = [f for f in fine if f.AssetClassification.MorningstarSectorCode == MorningstarSectorCode.ConsumerDefensive]
         self.consumerDefensive = sorted(filtered, key=lambda f: f.MarketCap, reverse=True)[:1]
         return [x.Symbol for x in self.technology + self.financialServices + self.consumerDefensive]
-
+        
 class MySectorWeightingPortfolioConstructionModel(EqualWeightingPortfolioConstructionModel):
 
-    # 2. In the constructor, pass in the rebalance parameter and set it to daily resolution
     def __init__(self, rebalance = Resolution.Daily):
-        # 3. Initialize the underlying class by passing it through
         super().__init__(rebalance)
-        # 4. Initialize an empty dictionary self.symbolBySectorCode to group the securities by sector code
-        self.symbolBySectorCode = {}
+        self.symbolBySectorCode = dict()
+
+    def OnSecuritiesChanged(self, algorithm, changes):
+        
+        for security in changes.AddedSecurities:
+            #1. When new assets are added to the universe, save the Morningstar sector code 
+            # for each security to the variable sectorCode
+            sectorCode = security.Fundamentals.AssetClassification
+            # 2. If the sectorCode is not in the self.symbolBySectorCode dictionary, create a new list 
+            # and append the symbol to the list, keyed by sectorCode in the self.symbolBySectorCode dictionary 
+            if sectorCode not in self.symbolBySectorCode:
+                self.symbolBySectorCode[sectorCode] = list()
+            self.symbolBySectorCode[sectorCode].append(security.Symbol)
+        for security in changes.RemovedSecurities:
+            #3. For securities that are removed, save their Morningstar sector code to sectorCode
+            
+            #4. If the sectorCode is in the self.symbolBySectorCode dictionary
+            if sectorCode in self.symbolBySectorCode:
+                symbol = security.Symbol
+                # If the symbol is in the dictionary's sectorCode list;
+                if symbol in sectorCode: 
+                    # Then remove the corresponding symbol from the dictionary
+                    self.symbolBySectorCode.remove(symbol)
+        # We use the super() function to avoid using the base class name explicity
+        super().OnSecuritiesChanged(algorithm, changes)
 
