7e6df6fd299a4e38bc602299345ab4ed1c637aa6
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-19 17:10:44 -0400

    Update 1-12-selecting-universe-by-sector.py

--- 1-12-selecting-universe-by-sector.py
+++ 1-12-selecting-universe-by-sector.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 from QuantConnect.Data.UniverseSelection import * 
 from Selection.FundamentalUniverseSelectionModel import FundamentalUniverseSelectionModel
+from Portfolio.EqualWeightingPortfolioConstructionModel import EqualWeightingPortfolioConstructionModel
 
 class SectorBalancedPortfolioConstruction(QCAlgorithm):
 
@@ -10,9 +11,10 @@ class SectorBalancedPortfolioConstruction(QCAlgorithm):
         self.SetCash(100000) 
 
         self.UniverseSettings.Resolution = Resolution.Hour
-        #1. Set an instance of MyUniverseSelectionModel using self.SetUniverseSelection
         self.SetUniverseSelection(MyUniverseSelectionModel())
         self.SetAlpha(ConstantAlphaModel(InsightType.Price, InsightDirection.Up, timedelta(1), 0.025, None))
+        # 1. Set an instance of the MySectorWeightingPortfolioConstructionModel using self.SetPortfolioConstruction
+        self.SetProtfolioConstruction(MySectorWeightingPortfolioConstructionModel())
         self.SetExecution(ImmediateExecutionModel())
 
 class MyUniverseSelectionModel(FundamentalUniverseSelectionModel):
@@ -21,18 +23,25 @@ class MyUniverseSelectionModel(FundamentalUniverseSelectionModel):
         super().__init__(True, None, None)
 
     def SelectCoarse(self, algorithm, coarse):
-        filtered = [x for x in coarse if x.HasFundamentalData > 0 and x.Price > 0]
+        filtered = [x for x in coarse if x.HasFundamentalData and x.Price > 0]
         sortedByDollarVolume = sorted(filtered, key=lambda x: x.DollarVolume, reverse=True)
         return [x.Symbol for x in sortedByDollarVolume][:100]
 
     def SelectFine(self, algorithm, fine):
-        #2. Save the top 3 securities sorted by MarketCap for the Technology sector to the variable self.technology
-        filtered = [x for x in fine if x.AssetClassification.MorningstarSectorCode == MorningstarSectorCode.Technology]
+        filtered = [f for f in fine if f.AssetClassification.MorningstarSectorCode == MorningstarSectorCode.Technology]
         self.technology = sorted(filtered, key=lambda f: f.MarketCap, reverse=True)[:3]
-        #3. Save the top 2 securities sorted by MarketCap for the Financial Services sector to the variable self.financialServices
-        filtered = [x for x in fine if x.AssetClassification.MorningstarSectorCode == MorningstarSectorCode.FinancialServices]
+        filtered = [f for f in fine if f.AssetClassification.MorningstarSectorCode == MorningstarSectorCode.FinancialServices]
         self.financialServices = sorted(filtered, key=lambda f: f.MarketCap, reverse=True)[:2]
-        #4. Save the top 1 securities sorted by MarketCap for the Consumer Goods sector to the variable self.consumerDefensive
-        filtered = [x for x in fine if x.AssetClassification.MorningstarSectorCode == MorningstarSectorCode.ConsumerDefensive]
+        filtered = [f for f in fine if f.AssetClassification.MorningstarSectorCode == MorningstarSectorCode.ConsumerDefensive]
         self.consumerDefensive = sorted(filtered, key=lambda f: f.MarketCap, reverse=True)[:1]
         return [x.Symbol for x in self.technology + self.financialServices + self.consumerDefensive]
+
+class MySectorWeightingPortfolioConstructionModel(EqualWeightingPortfolioConstructionModel):
+
+    # 2. In the constructor, pass in the rebalance parameter and set it to daily resolution
+    def __init__(self, rebalance = Resolution.Daily):
+        # 3. Initialize the underlying class by passing it through
+        super().__init__(rebalance)
+        # 4. Initialize an empty dictionary self.symbolBySectorCode to group the securities by sector code
+        self.symbolBySectorCode = {}
+
