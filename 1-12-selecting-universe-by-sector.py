fc2c092c01e294233527ba0abbd75cb571128b68
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-19 16:45:56 -0400

    Update 1-12-selecting-universe-by-sector.py

--- 1-12-selecting-universe-by-sector.py
+++ 1-12-selecting-universe-by-sector.py
@@ -1,1 +1,39 @@
 
+from datetime import timedelta
+from QuantConnect.Data.UniverseSelection import * 
+from Selection.FundamentalUniverseSelectionModel import FundamentalUniverseSelectionModel
+
+class SectorBalancedPortfolioConstruction(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2016, 12, 28) 
+        self.SetEndDate(2017, 3, 1) 
+        self.SetCash(100000) 
+
+        self.UniverseSettings.Resolution = Resolution.Hour
+        #1. Set an instance of MyUniverseSelectionModel using self.SetUniverseSelection
+        self.SetUniverseSelection(MyUniverseSelectionModel())
+        self.SetAlpha(ConstantAlphaModel(InsightType.Price, InsightDirection.Up, timedelta(1), 0.025, None))
+        self.SetExecution(ImmediateExecutionModel())
+
+class MyUniverseSelectionModel(FundamentalUniverseSelectionModel):
+
+    def __init__(self):
+        super().__init__(True, None, None)
+
+    def SelectCoarse(self, algorithm, coarse):
+        filtered = [x for x in coarse if x.HasFundamentalData > 0 and x.Price > 0]
+        sortedByDollarVolume = sorted(filtered, key=lambda x: x.DollarVolume, reverse=True)
+        return [x.Symbol for x in sortedByDollarVolume][:100]
+
+    def SelectFine(self, algorithm, fine):
+        #2. Save the top 3 securities sorted by MarketCap for the Technology sector to the variable self.technology
+        filtered = [x for x in fine if x.AssetClassification.MorningstarSectorCode == MorningstarSectorCode.Technology]
+        self.technology = sorted(filtered, key=lambda f: f.MarketCap, reverse=True)[:3]
+        #3. Save the top 2 securities sorted by MarketCap for the Financial Services sector to the variable self.financialServices
+        filtered = [x for x in fine if x.AssetClassification.MorningstarSectorCode == MorningstarSectorCode.FinancialServices]
+        self.technology = sorted(filtered, key=lambda f: f.MarketCap, reverse=True)[:2]
+        #4. Save the top 1 securities sorted by MarketCap for the Consumer Goods sector to the variable self.consumerDefensive
+        filtered = [x for x in fine if x.AssetClassification.MorningstarSectorCode == MorningstarSectorCode.ConsumerDefensive]
+        self.technology = sorted(filtered, key=lambda f: f.MarketCap, reverse=True)[:1]
+        return [x.Symbol for x in self.technology + self.financialServices + self.consumerDefensive]
