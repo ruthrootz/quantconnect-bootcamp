c8d990753efe67e64303da9a01ff2cd1c0cbf045
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-21 10:03:53 -0400

    Update 3-1-buy-and-hold-futures.py

--- 3-1-buy-and-hold-futures.py
+++ 3-1-buy-and-hold-futures.py
@@ -1,4 +1,6 @@
-class BootCampTask(QCAlgorithm):
+from math import floor
+
+class BasicTemplateFuturesAlgorithm(QCAlgorithm):
 
     def Initialize(self):
         self.SetStartDate(2013, 12, 20) 
@@ -6,22 +8,32 @@ class BootCampTask(QCAlgorithm):
         self.SetCash(1000000) 
         self.gold = self.AddFuture(Futures.Metals.Gold) 
         self.gold.SetFilter(0, 90)
-      
+ 
+    def OnMarginCallWarning(self):
+        self.Error("This is a margin call warning. The assets will be liquidated to cover losses.")
+        
     def OnData(self, slice):
-       
-        # Loop over each available futures chain from slice.FutureChains data
+      
         for chain in slice.FutureChains:
-            
-            #1. Filter to choose popular contracts with OpenInterest > 1000
-            self.popularContracts = [x for x in chain.Value if x.OpenInterest > 1000]
-            
-            #2. If the length of contracts in this chain is zero, continue to the next chain
+            self.popularContracts = [contract for contract in chain.Value if contract.OpenInterest > 1000]
+ 
             if len(self.popularContracts) == 0:
                 continue
             
-            #3. Sort our contracts by open interest in descending order and save to sortedByOIContracts
             sortedByOIContracts = sorted(self.popularContracts, key=lambda k : k.OpenInterest, reverse=True)
-            
-            #4. Save the contract with the highest open interest to self.liquidContract
             self.liquidContract = sortedByOIContracts[0]
-            
+  
+            if not self.Portfolio.Invested:
+                
+                #1. Save the notional value of the futures contract to self.notionalValue  
+                self.notionalValue = self.liquidContract.AskPrice * self.liquidContract.SymbolProperties.ContractMultiplier
+                
+                #2. Save the contract security object to the variable future
+                future = self.Securities[self.liquidContract.Symbol]
+                
+                #3. Calculate the number of contracts we can afford based on the margin required
+                # Divide the margin remaining by the initial margin and save to self.contractsToBuy
+                self.contractsToBuy = floor(self.Portfolio.MarginRemaining / future.BuyingPowerModel.InitialOvernightMarginRequirement)
+                
+                #4. Make a market order for the number of contracts we calculated for that symbol
+                self.MarketOrder(liquidContract.Symbol, self.contractsToBuy)
