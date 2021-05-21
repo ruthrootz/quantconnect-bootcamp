4cb21aec7f940558ff0f00679077411239296e86
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-21 09:47:47 -0400

    Update 3-1-buy-and-hold-futures.py

--- 3-1-buy-and-hold-futures.py
+++ 3-1-buy-and-hold-futures.py
@@ -4,9 +4,24 @@ class BootCampTask(QCAlgorithm):
         self.SetStartDate(2013, 12, 20) 
         self.SetEndDate(2014, 2, 20) 
         self.SetCash(1000000) 
-   
-        #1. Request Gold futures and save the gold security
-        self.gold = self.AddFuture(Futures.Metals.Gold)
-        
-        #2. Set our expiry filter to return all contracts expiring within 90 days
+        self.gold = self.AddFuture(Futures.Metals.Gold) 
         self.gold.SetFilter(0, 90)
+      
+    def OnData(self, slice):
+       
+        # Loop over each available futures chain from slice.FutureChains data
+        for chain in slice.FutureChains:
+            
+            #1. Filter to choose popular contracts with OpenInterest > 1000
+            self.popularContracts = [x for x in chain if x.OpenInterest > 1000]
+            
+            #2. If the length of contracts in this chain is zero, continue to the next chain
+            if len(self.popularContracts) == 0:
+                break
+            
+            #3. Sort our contracts by open interest in descending order and save to sortedByOIContracts
+            sortedByOIContracts = sorted(self.popularContracts, key=lambda k : k.OpenInterest, reverse=True)
+            
+            #4. Save the contract with the highest open interest to self.liquidContract
+            self.liquidContract = sortedByOIContracts[0]
+            
