200d04cc4a2e24a080c993e0b0ccf8912217ab95
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-21 10:09:18 -0400

    Update 3-1-buy-and-hold-futures.py

--- 3-1-buy-and-hold-futures.py
+++ 3-1-buy-and-hold-futures.py
@@ -8,32 +8,28 @@ class BasicTemplateFuturesAlgorithm(QCAlgorithm):
         self.SetCash(1000000) 
         self.gold = self.AddFuture(Futures.Metals.Gold) 
         self.gold.SetFilter(0, 90)
- 
+        
+        #1. Widen the free portfolio percentage to 30% to avoid margin calls for futures
+        self.Settings.FreePortfolioValuePercentage = 0.3
+    
     def OnMarginCallWarning(self):
-        self.Error("This is a margin call warning. The assets will be liquidated to cover losses.")
+        self.Error("You received a margin call warning..")
         
     def OnData(self, slice):
-      
+       
         for chain in slice.FutureChains:
             self.popularContracts = [contract for contract in chain.Value if contract.OpenInterest > 1000]
- 
+  
             if len(self.popularContracts) == 0:
                 continue
-            
+    
             sortedByOIContracts = sorted(self.popularContracts, key=lambda k : k.OpenInterest, reverse=True)
             self.liquidContract = sortedByOIContracts[0]
-  
+           
             if not self.Portfolio.Invested:
-                
-                #1. Save the notional value of the futures contract to self.notionalValue  
-                self.notionalValue = self.liquidContract.AskPrice * self.gold.SymbolProperties.ContractMultiplier
-                
-                #2. Save the contract security object to the variable future
-                future = self.Securities[self.liquidContract.Symbol]
-                
-                #3. Calculate the number of contracts we can afford based on the margin required
-                # Divide the margin remaining by the initial margin and save to self.contractsToBuy
-                self.contractsToBuy = floor(self.Portfolio.MarginRemaining / future.BuyingPowerModel.InitialOvernightMarginRequirement)
-                
-                #4. Make a market order for the number of contracts we calculated for that symbol
-                self.MarketOrder(self.liquidContract.Symbol, self.contractsToBuy)
+            
+                #3. Replace the manually calculated math above with 
+                # the SetHoldings function to automatically calculate your position size
+                self.SetHoldings(self.liquidContract.Symbol, -0.3)
+        
+    
