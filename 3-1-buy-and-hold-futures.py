0424fa4760a60d23c0db1e0c615dbee5400252fe
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-05-21 09:41:55 -0400

    Update 3-1-buy-and-hold-futures.py

--- 3-1-buy-and-hold-futures.py
+++ 3-1-buy-and-hold-futures.py
@@ -1,1 +1,12 @@
+class BootCampTask(QCAlgorithm):
 
+    def Initialize(self):
+        self.SetStartDate(2013, 12, 20) 
+        self.SetEndDate(2014, 2, 20) 
+        self.SetCash(1000000) 
+   
+        #1. Request Gold futures and save the gold security
+        self.gold = Futures.Metals.Gold
+        
+        #2. Set our expiry filter to return all contracts expiring within 90 days
+        self.gold.SetFilter(0, 90)
